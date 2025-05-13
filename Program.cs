4518dd75429aef829f7e9db0dbddd7b928aa1036
Rafael Lillo <fine.bulgur_0i@icloud.com>
2025-05-13 22:50:09 +0100

    Init project

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Paramore.Brighter;
+using Paramore.Brighter.Extensions.DependencyInjection;
+using Paramore.Brighter.MessagingGateway.Redis;
+using Paramore.Brighter.ServiceActivator.Extensions.DependencyInjection;
+using Paramore.Brighter.ServiceActivator.Extensions.Hosting;
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateLogger();
+
+var host = new HostBuilder()
+    .UseSerilog()
+    .ConfigureServices(
+        (_, services) =>
+        {
+            var connection = new RedisMessagingGatewayConfiguration
+            {
+                RedisConnectionString = "localhost:6379?connectTimeout=1&sendTimeout=1000&",
+                MaxPoolSize = 10,
+                MessageTimeToLive = TimeSpan.FromMinutes(10)
+            };
+
+            services
+                .AddHostedService<ServiceActivatorHostedService>()
+                .AddServiceActivator(opt =>
+                {
+                    opt.Subscriptions =
+                    [
+                        new RedisSubscription<Greeting>(
+                            new SubscriptionName("kafka.greeting.subscription"),
+                            new ChannelName("greeting.topic"),
+                            new RoutingKey("greeting.topic"),
+                            makeChannels: OnMissingChannel.Create
+                        ),
+                    ];
+
+                    opt.ChannelFactory = new ChannelFactory(
+                        new RedisMessageConsumerFactory(connection)
+                    );
+                })
+                .AutoFromAssemblies()
+                .UseExternalBus(
+                    new RedisProducerRegistryFactory(
+                        connection,
+                        [
+                            new RedisMessagePublication 
+                            {
+                                MakeChannels = OnMissingChannel.Create,
+                                Topic = new RoutingKey("greeting.topic"),
+                            },
+                        ]
+                    ).Create()
+                );
+        }
+    )
+    .Build();
+
+
+await host.StartAsync();
+
+while (true)
+{
+    await Task.Delay(TimeSpan.FromSeconds(10));
+    Console.Write("Say your name (or q to quit): ");
+    var name = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(name))
+    {
+        continue;
+    }
+
+    if (name == "q")
+    {
+        break;
+    }
+
+    var process = host.Services.GetRequiredService<IAmACommandProcessor>();
+    process.Post(new Greeting { Name = name });
+}
+
+await host.StopAsync();
+
+public class Greeting() : Event(Guid.NewGuid())
+{
+    public string Name { get; set; } = string.Empty;
+}
+
+public class GreetingMapper : IAmAMessageMapper<Greeting>
+{
+    public Message MapToMessage(Greeting request)
+    {
+        var header = new MessageHeader();
+        header.Id = request.Id;
+        header.TimeStamp = DateTime.UtcNow;
+        header.Topic = "greeting.topic";
+        header.MessageType = MessageType.MT_EVENT;
+
+        var body = new MessageBody(JsonSerializer.Serialize(request));
+        return new Message(header, body);
+    }
+
+    public Greeting MapToRequest(Message message)
+    {
+        return JsonSerializer.Deserialize<Greeting>(message.Body.Bytes)!;
+    }
+}
+
+public class GreetingHandler(ILogger<GreetingHandler> logger) : RequestHandler<Greeting>
+{
+    public override Greeting Handle(Greeting command)
+    {
+        logger.LogInformation("Hello {Name}", command.Name);
+        return base.Handle(command);
+    }
+}
