a59712ef35953b755b135a8d6ece3eebcf3d3fa7
Rafael Andrade <rafael.andrade@justeattakeaway.com>
2025-11-10 13:15:23 +0000

    Update Sample

--- Program.cs
+++ Program.cs
@@ -1,39 +1,31 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Fluent.Brighter;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Npgsql;
 using Paramore.Brighter;
-using Paramore.Brighter.Extensions.DependencyInjection;
 using Paramore.Brighter.Inbox.Postgres;
-using Paramore.Brighter.MessagingGateway.RMQ.Async;
-using Paramore.Brighter.ServiceActivator.Extensions.DependencyInjection;
+using Paramore.Brighter.MessagingGateway.Kafka;
+using Paramore.Brighter.Policies.Attributes;
 using Paramore.Brighter.ServiceActivator.Extensions.Hosting;
+using Polly;
+using Polly.Retry;
 using Serilog;
 
+const string kafkaPolicy = "kafka-policy";
 const string connectionString = "Host=localhost;Username=postgres;Password=password;Database=brightertests;";
 await using (NpgsqlConnection connection = new(connectionString))
 {
     await connection.OpenAsync();
     await using var command = connection.CreateCommand();
-    command.CommandText =
-      """
-      CREATE TABLE IF NOT EXISTS "inboxmessages"
-      (
-        CommandId VARCHAR(256) NOT NULL ,
-        CommandType VARCHAR(256) NULL ,
-        CommandBody TEXT NULL ,
-        Timestamp timestamptz  NULL ,
-        ContextKey VARCHAR(256) NULL,
-        PRIMARY KEY (CommandId, ContextKey)
-      ); 
-      """;
-
+    
+    command.CommandText = PostgreSqlInboxBuilder.GetDDL("inboxmessages");
     _ = await command.ExecuteNonQueryAsync();
 }
 
 Log.Logger = new LoggerConfiguration()
     .MinimumLevel.Information()
-    .MinimumLevel.Override("Paramore.Brighter", Serilog.Events.LogEventLevel.Debug)
+    .MinimumLevel.Override("Paramore.Brighter", Serilog.Events.LogEventLevel.Warning)
     .Enrich.FromLogContext()
     .WriteTo.Console()
     .CreateLogger();
@@ -43,59 +35,104 @@ var host = new HostBuilder()
     .ConfigureServices(
         (_, services) =>
         {
-            var connection = new RmqMessagingGatewayConnection
-            {
-                AmpqUri = new AmqpUriSpecification(new Uri("amqp://guest:guest@localhost:5672")),
-                Exchange = new Exchange("paramore.brighter.exchange"),
-            };
-
             services
                 .AddHostedService<ServiceActivatorHostedService>()
-                .AddConsumers(opt =>
-                {
-                    opt.InboxConfiguration = new InboxConfiguration(
-                        new PostgreSqlInbox(new RelationalDatabaseConfiguration(connectionString, "brightertests", inboxTableName: "inboxmessages")));
-                    
-                    opt.Subscriptions =
-                    [
-                        new RmqSubscription<OrderPlaced>(
-                            new SubscriptionName("subscription-orderplaced"),
-                            new ChannelName("order-placed-queue"),
-                            new RoutingKey("order-placed-topic"),
-                            makeChannels: OnMissingChannel.Create,
-                            messagePumpType: MessagePumpType.Reactor
-                        ),
+                .AddFluentBrighter(opt => opt
+                    .Subscriptions(c => c.AddResiliencePipeline<object>(kafkaPolicy, (builder, _) => builder
+                        .AddRetry(new RetryStrategyOptions
+                        {
+                            Delay = TimeSpan.FromSeconds(1),
+                            MaxRetryAttempts = 3
+                        })))
+                    .UsingPostgres(pg => pg
+                        .SetConnection(db => db
+                            .SetConnectionString(connectionString)
+                            .SetDatabaseName("brightertests")
+                            .SetInboxTableName("inboxmessages"))
+                        .UseInbox())
+                    .UsingKafka(kf => kf
+                        .SetConnection(c => c
+                            .SetName("sample")
+                            .SetBootstrapServers("localhost:9092")
+                            .SetSecurityProtocol(SecurityProtocol.Plaintext)
+                            .SetSaslMechanisms(SaslMechanism.Plain))
+                        .UseSubscriptions(s => s
+                            .AddSubscription<OrderPlaced>(sb => sb
+                                .SetTopic("order-placed-topic")
+                                .SetConsumerGroupId("order-placed-topic-1")
+                                .SetRequeueCount(3)
+                                .CreateInfrastructureIfMissing()
+                                .UseReactorMode())
+                            .AddSubscription<OrderPaid>(sb => sb
+                                .SetTopic("order-paid-topic")
+                                .SetConsumerGroupId("order-paid-topic-1")
+                                .CreateInfrastructureIfMissing()
+                                .UseReactorMode()))
+                        .UsePublications(p => p
+                            .AddPublication<OrderPaid>(kp => kp
+                                .SetTopic("order-paid-topic")
+                                .CreateTopicIfMissing())
+                            .AddPublication<OrderPlaced>(kp => kp
+                                .SetTopic("order-placed-topic")
+                                .CreateTopicIfMissing()))));
 
-                        new RmqSubscription<OrderPaid>(
-                            new SubscriptionName("subscription-orderpaid"),
-                            new ChannelName("order-paid-queue"),
-                            new RoutingKey("order-paid-topic"),
-                            makeChannels: OnMissingChannel.Create,
-                            messagePumpType: MessagePumpType.Reactor
-                        ),
-                    ];
-                    
-                    opt.DefaultChannelFactory = new ChannelFactory(new RmqMessageConsumerFactory(connection));
-                })
-                .AutoFromAssemblies()
-                .AddProducers(opt =>
-                {
-                    opt.ProducerRegistry = new RmqProducerRegistryFactory(
-                        connection,
-                        [
-                            new RmqPublication<OrderPaid>
-                            {
-                                MakeChannels = OnMissingChannel.Create,
-                                Topic = new RoutingKey("order-paid-topic"),
-                            },
-                            new RmqPublication<OrderPlaced>
-                            {
-                                MakeChannels = OnMissingChannel.Create,
-                                Topic = new RoutingKey("order-placed-topic"),
-
-                            },
-                        ]).Create();
-                });
+            // var connection = new KafkaMessagingGatewayConfiguration
+            // {
+            //     Name = "sample",
+            //     BootStrapServers = ["localhost:9092"],
+            //     SaslUsername = "admin",
+            //     SaslPassword = "admin-secret",
+            //     SecurityProtocol = SecurityProtocol.Plaintext,
+            //     SaslMechanisms = SaslMechanism.Plain,
+            // };
+            //
+            // services
+            //     .AddHostedService<ServiceActivatorHostedService>()
+            //     .AddConsumers(opt =>
+            //     {
+            //         opt.InboxConfiguration = new InboxConfiguration(new PostgreSqlInbox(new RelationalDatabaseConfiguration(connectionString, "brightertests", inboxTableName: "inboxmessages")));
+            //         
+            //         opt.Subscriptions =
+            //         [
+            //             new KafkaSubscription<OrderPlaced>(
+            //                 new SubscriptionName("subscription-orderplaced"),
+            //                 new ChannelName("order-placed-queue"),
+            //                 new RoutingKey("order-placed-topic"),
+            //                 makeChannels: OnMissingChannel.Create,
+            //                 messagePumpType: MessagePumpType.Reactor,
+            //                 groupId: "test"
+            //             ),
+            //
+            //             new KafkaSubscription<OrderPaid>(
+            //                 new SubscriptionName("subscription-orderpaid"),
+            //                 new ChannelName("order-paid-queue"),
+            //                 new RoutingKey("order-paid-topic"),
+            //                 makeChannels: OnMissingChannel.Create,
+            //                 messagePumpType: MessagePumpType.Reactor,
+            //                 groupId: "test"
+            //             ),
+            //         ];
+            //         
+            //         opt.DefaultChannelFactory = new ChannelFactory(new KafkaMessageConsumerFactory(connection));
+            //     })
+            //     .AutoFromAssemblies()
+            //     .AddProducers(opt =>
+            //     {
+            //         opt.ProducerRegistry = new KafkaProducerRegistryFactory(
+            //             connection,
+            //             [
+            //                 new KafkaPublication<OrderPaid>
+            //                 {
+            //                     MakeChannels = OnMissingChannel.Create,
+            //                     Topic = new RoutingKey("order-paid-topic"),
+            //                 },
+            //                 new KafkaPublication<OrderPlaced>
+            //                 {
+            //                     MakeChannels = OnMissingChannel.Create,
+            //                     Topic = new RoutingKey("order-placed-topic"),
+            //                 }
+            //             ]).Create();
+            //     });
         }
     )
     .Build();
@@ -130,7 +167,7 @@ while (!cancellationTokenSource.IsCancellationRequested)
     {
         using var scope = host.Services.CreateScope();
         var process = scope.ServiceProvider.GetRequiredService<IAmACommandProcessor>();
-        await process.SendAsync(new CreateNewOrder { Value = value });
+        process.Send(new CreateNewOrder { Value = value });
     }
     catch(Exception ex)
     {
@@ -159,23 +196,18 @@ public class OrderPaid() : Event(Id.Random())
 }
 
 public class CreateNewOrderHandler(IAmACommandProcessor commandProcessor,
-    ILogger<CreateNewOrderHandler> logger) : RequestHandlerAsync<CreateNewOrder>
+    ILogger<CreateNewOrderHandler> logger) : RequestHandler<CreateNewOrder>
 {
-    public override async Task<CreateNewOrder> HandleAsync(CreateNewOrder command, CancellationToken cancellationToken = default)
+    public override CreateNewOrder Handle(CreateNewOrder command)
     {
         try
         {
             var id = Uuid.NewAsString();
             logger.LogInformation("Creating a new order: {OrderId}", id);
 
-            await commandProcessor.DepositPostAsync(new OrderPlaced { OrderId = id, Value = command.Value }, cancellationToken: cancellationToken);
-            if (command.Value % 3 == 0)
-            {
-                throw new InvalidOperationException("invalid value");
-            }
-
-            await commandProcessor.DepositPostAsync(new OrderPaid { OrderId = id }, cancellationToken: cancellationToken);
-            return await base.HandleAsync(command, cancellationToken);
+            commandProcessor.Post(new OrderPlaced { OrderId = id, Value = command.Value });
+            commandProcessor.Post(new OrderPaid { OrderId = id });
+            return base.Handle(command);
         }
         catch (Exception ex)
         {
@@ -187,9 +219,16 @@ public class CreateNewOrderHandler(IAmACommandProcessor commandProcessor,
 
 public class OrderPlaceHandler(ILogger<OrderPlaceHandler> logger) : RequestHandler<OrderPlaced>
 {
+    [UseResiliencePipeline("kafka-policy", 1)]
     public override OrderPlaced Handle(OrderPlaced command)
     {
         logger.LogInformation("{OrderId} placed with value {OrderValue}", command.OrderId, command.Value);
+        if (command.Value % 3 == 0)
+        {
+            logger.LogError("Simulate an error for {OrderId} with value {OrderValue}", command.OrderId, command.Value);
+            throw new InvalidOperationException("invalid error");
+        }
+        
         return base.Handle(command);
     }
 }
